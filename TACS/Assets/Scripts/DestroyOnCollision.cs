6559ff60d2664a78746411f380dffbf124bd6c2f
SStepanik <seanstepanik@gmail.com>
2024-10-19 14:14:36 -0500

    changed destruction

--- TACS/Assets/Scripts/DestroyOnCollision.cs
+++ TACS/Assets/Scripts/DestroyOnCollision.cs
@@ -2,26 +2,26 @@ using UnityEngine;
 
 public class DestroyOnCollision : MonoBehaviour
 {
-    // Reference to the score manager (or you can handle the score here directly)
     public ScoreManager scoreManager;
-    // This function is called when the object collides with another
+
     void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Moveable object collided with destroyer");
-        // Check if the object colliding has the tag "Moveable"
         if (collision.gameObject.CompareTag("Moveable"))
         {
-            Debug.Log("Destroyer tag matched, destroying moveable object");
-            // Check if this object has the tag "Destroyer"
+            Debug.Log("Moveable object collided with destroyer");
+
             if (gameObject.CompareTag("Destroyer"))
             {
-                // Destroy the moveable object
-                Destroy(collision.gameObject);
+                Debug.Log("Destroyer tag matched, deactivating moveable object");
+
+                // Deactivate the moveable object instead of destroying it
+                collision.gameObject.SetActive(false);
 
                 // Increase the score
                 if (scoreManager != null)
                 {
-                    scoreManager.IncreaseScore(1);  // Increase the score by 1
+                    scoreManager.IncreaseScore(1);
+                    Debug.Log("Score increased, current score: " + scoreManager.score);
                 }
             }
         }
