4c5e4840ad876d47595b51330a2fb39576094772
SStepanik <seanstepanik@gmail.com>
2024-11-06 12:15:18 -0600

    adding human cleaning up scene

--- TACS/Assets/Scripts/MoveableObject.cs
+++ TACS/Assets/Scripts/MoveableObject.cs
@@ -7,13 +7,25 @@ public class MoveableObject : MonoBehaviour
     private SpriteRenderer sprite_renderer;
     private Rigidbody2D rigid_body;
     public ScoreManager scoreManager;
+    private Vector2 startpos;
+    public LayerMask humanFieldLayer;
 
     private void Awake()
     {
         sprite_renderer = GetComponent<SpriteRenderer>();
         rigid_body = GetComponent<Rigidbody2D>();
+        startpos = this.gameObject.transform.position;
     }
     // Start is called before the first frame update
+
+    void Update ()
+    {
+        if(Physics2D.OverlapCircle(this.gameObject.transform.position, .1f, humanFieldLayer)) { //add result
+            Debug.Log("You've been spotted! block!");
+            this.gameObject.transform.position = startpos;
+            Debug.Log("startposition"+startpos);
+        }
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -32,6 +44,6 @@ public class MoveableObject : MonoBehaviour
                 }
                 
             }
-        }
+        }         
     }
 }
