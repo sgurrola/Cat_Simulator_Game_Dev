bbdba74fb28d41c38985381fae8155defed800b4
sgurrola <124643885+sgurrola@users.noreply.github.com>
2024-11-07 18:51:12 -0600

    almost
    Merged Justin, Sean, and Sandra

--- TACS/Assets/Scripts/HumanMovement.cs
+++ TACS/Assets/Scripts/HumanMovement.cs
@@ -14,6 +14,8 @@ public class HumanMovement : MonoBehaviour
     public GameObject humanReset;
     public LayerMask safeZoneLayer;
 
+    bool hasRotated = false;
+
     //public float hTimer = 15f;
 
     void Start()
@@ -28,7 +30,7 @@ public class HumanMovement : MonoBehaviour
                 Debug.Log("You've been spotted!");
             //scene reset
             playerCaught = true;
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
             }
             Debug.Log("Good Hiding");
         }
@@ -43,23 +45,29 @@ public class HumanMovement : MonoBehaviour
         {
             transform.position = new Vector3(-23.5f, -2f, 0f);
             humanField.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            hasRotated = false;
+
             
             speed = 0;
         }
         GameObject[] boxes = GameObject.FindGameObjectsWithTag("Moveable");
+        GameObject[] bombs = GameObject.FindGameObjectsWithTag("Bomb");
         foreach (GameObject box in boxes)
         {
-            Rigidbody2D boxRigidbody = box.GetComponent<Rigidbody2D>();
+            foreach(GameObject bomb in bombs) {
+                Rigidbody2D boxRigidbody = box.GetComponent<Rigidbody2D>();
 
-        // Check if the box has a Rigidbody2D and its speed is greater than 0.1
-            if (boxRigidbody != null && boxRigidbody.velocity.magnitude > 0.1f)
-            {
-            if (Physics2D.OverlapCircle(box.transform.position, 0.1f, humanFieldLayer))
-            {
-                // Rotate the humanField object
-                humanField.transform.Rotate(0f, 0f, .02f, Space.Self);
-                Debug.Log("Box has moved!");
-            }
+            // Check if the box has a Rigidbody2D and its speed is greater than 0.1
+                if (boxRigidbody != null && boxRigidbody.velocity.magnitude > 1f)
+                {
+                if (Physics2D.OverlapCircle(box.transform.position, 0.1f, humanFieldLayer) && !hasRotated)
+                {
+                    // Rotate the humanField object
+                    humanField.transform.Rotate(0f, 0f, .02f, Space.Self);
+                    Debug.Log("Box has moved!");
+                    hasRotated = true;
+                }
+                }
             }
         }
     }
